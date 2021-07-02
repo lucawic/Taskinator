ad196a794f1dc71ca633479ab4602f2abd697ba0
lucawic <lucaswick@email.arizona.edu>
2021-07-02 07:23:19 -0700

    i got this to work finally

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,22 @@
-//allows you to execute event with the enter key
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var taskFormHandler = function (event) {
+var taskFormHandler = function(event) {
     event.preventDefault();
     var taskNameInput = document.querySelector("input[name='task-name']").value;
     var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //check if input values are empty strings
+    if (taskNameInput === "" || taskTypeInput === "") {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+
+    formEl.reset();
+    
+    //reset form fields for next task to be entered
+    document.querySelector("input[name='task-name']").value =" ";
+    document.querySelector("select[name='task-type']").selectedIndex = 0;
 
     //package up data as an object
     var taskDataObj = {
@@ -14,24 +25,24 @@ var taskFormHandler = function (event) {
     };
     
     //send it as an argument to createTaskEl
-    createTaskEl(taskDataObj)
+    createTaskEl(taskDataObj);
 };
 
 // code to createa a new task HTML element
 var createTaskEl = function(taskDataObj) {
     // create list item
-var listItemEl = document.createElement("li");
-listItemEl.className = "task-item";
+    var listItemEl = document.createElement("li");
+    listItemEl.className = "task-item";
 
 // create div to hold task info and add to list item
-var taskInfoEl = document.createElement("div");
-taskInfoEl.className = "task-info";
-taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    var taskInfoEl = document.createElement("div");
+    taskInfoEl.className = "task-info";
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+    listItemEl.appendChild(taskInfoEl);
 
-listItemEl.appendChild(taskInfoEl);
-
+    console.dir(listItemEl);
 // add entire list item to list
-tasksToDoEl.appendChild(listItemEl);
+    tasksToDoEl.appendChild(listItemEl);
 };
 
 formEl.addEventListener("submit", taskFormHandler);
